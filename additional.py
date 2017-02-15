e40a7316d8e9df7699ecf63744ceb40b7b6012c6
egorrozinskiy <egor.rozinskiy@mail.ru>
2017-02-15 18:23:04 +0300

    1

--- additional.py
+++ additional.py
@@ -1,3 +1,5 @@
 __author__ = 'student'
 s, m = tuple(map(int, input().split()))
 A = list(map(int,input().split()))
+check = [0 for i in range(len(A))]
+''' число подмножеств сумма элементов меньше с произведение меньше м'''
