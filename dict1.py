18c2e139275fa2fda5945f70dae4d7d9343a3601
egorrozinskiy <egor.rozinskiy@mail.ru>
2017-02-15 16:01:15 +0300

    1

--- dict1.py
+++ dict1.py
@@ -1,1 +1,13 @@
 __author__ = 'student'
+N = int(input())
+diploma = dict()
+attach = dict()
+for i in range(N):
+    a=input().split()
+    diploma[a[0]] = int(a[1])
+for j in range(N):
+    b = input().split()
+    attach[b[0]] = int(b[1])
+for p in diploma:
+    print(diploma[p], attach[p])
+
